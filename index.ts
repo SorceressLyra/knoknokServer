da411ad2e7142813b83d15491f7b8fdaa6c87c1e
Lyra <lyramikkelsen@gmail.com>
2025-02-08 18:18:42 +0100

    Changed a few things

--- index.ts
+++ index.ts
@@ -1,7 +1,16 @@
 Bun.serve({
-    fetch(req, server) {}, // upgrade logic
+    fetch(req, server) {
+      server.upgrade(req, {
+        data: {
+          createdAt: Date.now(),
+        }
+      });
+    }, // upgrade logic
     websocket: {
-      message(ws, message) {}, // a message is received
+      message(ws, message) {
+          console.log(message);
+          ws.send(message);
+      }, // a message is received
       open(ws) {}, // a socket is opened
       close(ws, code, message) {}, // a socket is closed
       drain(ws) {}, // the socket is ready to receive more data
