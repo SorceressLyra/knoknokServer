6195e5472d4fb3313cd46910333f1a9dd32e81eb
Lyra <lyramikkelsen@gmail.com>
2025-02-08 12:01:19 +0100

    Updated gitignore

--- index.ts
+++ index.ts
@@ -1,1 +1,9 @@
-console.log("Hello via Bun!");
+Bun.serve({
+    fetch(req, server) {}, // upgrade logic
+    websocket: {
+      message(ws, message) {}, // a message is received
+      open(ws) {}, // a socket is opened
+      close(ws, code, message) {}, // a socket is closed
+      drain(ws) {}, // the socket is ready to receive more data
+    },
+  });
