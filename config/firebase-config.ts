a29c4c20d3ee9ed2a97ada139d381e1956e62e37
Lyra <lyramikkelsen@gmail.com>
2025-03-09 10:17:56 +0100

    Move away from bun

--- config/firebase-config.ts
+++ config/firebase-config.ts
@@ -1,6 +1,9 @@
 import admin from 'firebase-admin';
 import path from 'path';
+import { fileURLToPath } from 'url';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const serviceAccountPath = path.join(__dirname, '../serviceAccountKey.json');
 
 export const initializeFirebase = () => {
